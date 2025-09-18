ccdddd0f4b1a48cf52ffc25d8798b64cddfecee3
CcProfessor <profes_mf@hotmail.com>
2025-09-18 00:38:41 +0000

    more

---
+++ core/rules/interfaces/client.interface.ts
@@ -0,0 +1,12 @@
+import { IsOptional, IsString, IsNumber, IsArray } from 'class-validator';
+
+export class EnterTargetDto {
+  @IsOptional() @IsNumber() screenWidth?: number;
+  @IsOptional() @IsNumber() screenHeight?: number;
+  @IsOptional() @IsString() timezone?: string;
+  @IsOptional() @IsString() language?: string;
+  @IsOptional() @IsArray() languages?: string[];
+  @IsOptional() @IsString() platform?: string;
+  @IsOptional() @IsNumber() deviceMemory?: number;
+  @IsOptional() @IsNumber() hardwareConcurrency?: number;
+}
