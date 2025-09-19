156cd3886d8fbd48090f48492213191c5da3854b
CcProfessor <profes_mf@hotmail.com>
2025-09-19 22:41:35 +0000

    more

--- player/src/components/target.components.tsx
+++ player/src/components/target.components.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useTarget } from "../contexts/target.context";
 import { CreateTargetDto } from "../rules/interfaces/target.interfaces";
 
