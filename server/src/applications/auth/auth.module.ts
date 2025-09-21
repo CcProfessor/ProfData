2fd08d66749b898d9143a3822311c5719492894b
CcProfessor <profes_mf@hotmail.com>
2025-09-21 17:01:07 +0000

    more

--- server/src/applications/auth/auth.module.ts
+++ server/src/applications/auth/auth.module.ts
@@ -1,28 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PlayerService } from '../player/player.service';
-import { PlayerRepository } from '../player/repository/player.repository';
-import { TargetService } from '../target/target.service';
-import { TargetRepository } from '../target/repository/target.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { TargetModule } from '../target/target.module';
+import { PlayerModule } from '../player/player.module';
 
 @Module({
   imports: [
+    PlayerModule,
+    TargetModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'super_secret_key', // 🔑 segredo do token
-      signOptions: { expiresIn: '1h' }, // token expira em 1h
+      secret: process.env.JWT_SECRET || 'super_secret_key',
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    PlayerService,
-    PlayerRepository,
-    TargetService,
-    TargetRepository,
-    JwtStrategy],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
