4a97c2d8fe7f669423a222b9525f040f831b7bcc
CcProfessor <profes_mf@hotmail.com>
2025-09-23 02:44:39 +0000

    more

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './applications/app.module';
 
+
+import { Logger } from '@nestjs/common'; // Temp
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -11,12 +14,26 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT_S ?? 3000);
 
-  const server = app.getHttpServer();
-  const router = server._events.request._router;
-  console.log(
-    router.stack
-      .filter(r => r.route)
-      .map(r => r.route.path)
-  );
+   const server = app.getHttpServer();
+  const address = await app.getUrl();
+  Logger.log(`🚀 Server is running at ${address}`);
+
+  // Lista rotas
+  const router = server._events.request?.router;
+  if (router) {
+    const routes = [];
+    router.stack.forEach((layer) => {
+      if (layer.route) {
+        const path = layer.route?.path;
+        const methods = Object.keys(layer.route.methods)
+          .map((m) => m.toUpperCase())
+          .join(', ');
+        routes.push(`${methods} ${path}`);
+      }
+    });
+    Logger.log('📌 Routes:\n' + routes.join('\n'));
+  } else {
+    Logger.warn('⚠️ Não consegui ler rotas via Express. Use um módulo como @nestjs/core/RouterExplorer');
+  }
 }
 bootstrap();
