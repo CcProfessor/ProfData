b0af8138027782f89e3e8c49a072ee6e762e24ae
CcProfessor <profes_mf@hotmail.com>
2025-09-23 02:29:31 +0000

    more

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,14 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      "https://turbo-space-yodel-974px7q6wjrw2x9gx-5175.app.github.dev",
-      "https://turbo-space-yodel-974px7q6wjrw2x9gx-5175.app.github.dev/login",
-      "https://turbo-space-yodel-974px7q6wjrw2x9gx-5174.app.github.dev",
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    allowedHeaders: "*",
   });
 
   await app.listen(process.env.PORT_S ?? 3000);
