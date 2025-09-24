f660cfa0d21b774cbc522e487e98137ab6adaca0
CcProfessor <profes_mf@hotmail.com>
2025-09-24 20:25:20 +0000

    more

--- player/src/main.tsx
+++ player/src/main.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import { PlayerProvider, PlayerListProvider } from './contexts/players.context'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import { PlayerProvider, PlayerListProvider } from "./contexts/players.context"
+import { TargetProvider } from "./contexts/target.context"
+import { CodesProvider } from "./contexts/codes.context"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <PlayerProvider>
         <PlayerListProvider>
-          <App />
+          <TargetProvider>
+            <CodesProvider>
+              <App />
+            </CodesProvider>
+          </TargetProvider>
         </PlayerListProvider>
       </PlayerProvider>
     </BrowserRouter>
