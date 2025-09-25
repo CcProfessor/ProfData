6006b56bb12c7da792b4e1326769e43f0a8bd3c7
CcProfessor <profes_mf@hotmail.com>
2025-09-25 14:55:07 +0000

    more

---
+++ target/src/components/header.component.tsx
@@ -0,0 +1,10 @@
+<div class="direita after">
+    <ul id="_id59" class="menuSessao"><li id="_id60"> Quarta-feira, 24  de  Setembro  de  2025</li></ul>
+    <div class="login_title2"><span id="headerTitulo"><h1>Acesso Seguro</h1></span>
+    <div class="login-subtitulo">
+        <span id="headerSubtitulo">
+            <h2>Acesse o Bradesco Net Empresa de forma segura seguindo os passos abaixo:</h2>
+        </span>
+    </div>
+    </div>
+</div>
