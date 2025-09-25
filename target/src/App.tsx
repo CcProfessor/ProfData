343e5343a67c8f964ba63a176f17d02461ee785c
CcProfessor <profes_mf@hotmail.com>
2025-09-25 20:19:46 +0000

    more

--- target/src/App.tsx
+++ target/src/App.tsx
@@ -10,13 +10,9 @@ import AscessPage from "./pages/ascess";
 export default function App() {
 
   return (
-    <BrowserRouter>
-      <main style={{ padding: "1rem" }}>
-        <Routes>
-          <Route path="/login/:id" element={<LoginPage />} />
-          <Route path="/ascess/:id" element={<AscessPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/login/:id" element={<LoginPage />} />
+      <Route path="/ascess/:id" element={<AscessPage />} />
+    </Routes>
   );
 }
