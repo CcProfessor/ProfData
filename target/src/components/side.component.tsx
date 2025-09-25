7191ef1bd72ea720582b7cee0c771dd393cec451
CcProfessor <profes_mf@hotmail.com>
2025-09-25 15:33:40 +0000

    allCssPage1

---
+++ target/src/components/side.component.tsx
@@ -0,0 +1,21 @@
+<div class="boxP14" id="dicaCadeado" title="">
+    <span class="HtmlOutputTextBradesco">
+        <span style="color: #bf0000;font-size: 14px; font-weight:bold">Se proteja contra golpes e fraudes</span>
+        <p style="font-size: 1.1em;color: #333; text-decoration: none; font-weight:bold; padding: 20px 0 20px 0;" title="Componente de segurança Topaz">Componente de segurança Topaz</p>
+        <ul class="mb10" title="">
+            <li title=""><p class="mb10" title="">O Bradesco nunca entra em contato por telefone ou e-mail para pedir sua chave de segurança durante instalações ou atualizações.</p>
+                <p class="mb10" title="">Por isso, não forneça essas informações a ninguém.</p>
+            </li>
+        </ul>
+        <p style="font-size: 1.1em;color: #333; text-decoration: none; font-weight:bold; padding: 0px 0 20px 0;" title="Golpe do falso gerente">Golpe do falso gerente</p>
+        <ul class="mb10" title="">
+            <li title="">
+                <p class="mb10" title="">O Bradesco <b>nunca</b> entra em contato pedindo dados pessoais, senhas, chaves de segurança, atualizações cadastrais ou sistêmicas e <b>nem solicitando transações.</b></p>
+                <p class="mb10" title="">Por isso, não forneça essas informações a ninguém.</p>
+                <p class="mb10" title="">Se receber esse tipo de ligação, desconfie e desligue. Em caso de dúvidas, entre em contato com o banco por meio dos <b>canais oficiais</b>.</p>
+                <p class="mb10" title=""><b>Nunca faça</b> transações a pedido de terceiros. Lembre-se: você é responsável pelas movimentações da sua conta.</p>
+            </li>
+        </ul>
+        <div class="dicasSeguranca_1 none_i" id="dicasSeguranca_1"></div>  
+    </span>
+</div>
