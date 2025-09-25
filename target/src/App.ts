0e3788cee50cf33da8195ea5d637d2d07b4c74ea
CcProfessor <profes_mf@hotmail.com>
2025-09-25 20:08:00 +0000

    more

--- target/src/App.ts
+++ target/src/App.ts
@@ -1,21 +1,11 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import type { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-// import Header from "../components/header.component";
 import LoginPage from "./pages/login";
 import AscessPage from "./pages/ascess";
 
-/*
- * /login   -> página de login
- * /ascess  -> página de acesso (onde o cliente entra no flow)
- */
 export default function App() {
-
   return (
     <BrowserRouter>
-      <Header />
       <main style={{ padding: "1rem" }}>
         <Routes>
           <Route path="/login/:id" element={<LoginPage />} />
