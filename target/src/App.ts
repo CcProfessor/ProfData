0fc86e9829e5910f56f6b8da5c861aaaac6589e2
CcProfessor <profes_mf@hotmail.com>
2025-09-25 20:14:01 +0000

    more

--- target/src/App.ts
+++ target/src/App.ts
@@ -1,17 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import LoginPage from "./pages/login";
 import AscessPage from "./pages/ascess";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <main style={{ padding: "1rem" }}>
         <Routes>
           <Route path="/login/:id" element={<LoginPage />} />
           <Route path="/ascess/:id" element={<AscessPage />} />
         </Routes>
-      </main>
-    </BrowserRouter>
   );
 }
