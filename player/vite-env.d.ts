ff2bbaad1935e7f6a07755042566098f853edae7
CcProfessor <profes_mf@hotmail.com>
2025-10-03 01:53:54 +0000

    ServerBranch

--- player/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_P_BASE_URL: string
-  readonly VITE_SERVER_URL: string
-  readonly VITE_T_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
